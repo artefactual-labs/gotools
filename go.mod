777d5256b1f66c83805bfc9e0206f6b41452bea5
Jesús García Crespo <jesus@sevein.com>
2023-06-28 16:30:18 +0000

    ref: add initial implementation

--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ go 1.20
 require (
 	github.com/go-logr/logr v1.2.4
 	github.com/go-logr/zapr v1.2.4
-	github.com/google/go-cmp v0.5.5
+	github.com/google/go-cmp v0.5.8
 	go.uber.org/zap v1.24.0
+	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
 	gotest.tools/v3 v3.4.0
 )
 
 require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
---
+++ ref/ref_test.go
@@ -0,0 +1,96 @@
+package ref_test
+
+import (
+	"testing"
+
+	"gotest.tools/v3/assert"
+
+	"go.artefactual.dev/tools/ref"
+)
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	s := "string"
+
+	p1 := ref.New(s)
+	assert.Equal(t, s, *p1)
+	assert.Assert(t, &s != p1)
+
+	p2 := ref.New(s)
+	assert.Equal(t, s, *p2)
+	assert.Assert(t, &s != p2)
+}
+
+func TestNewNillable(t *testing.T) {
+	t.Parallel()
+
+	s := "string"
+	ptr := ref.NewNillable(s)
+	assert.Equal(t, *ptr, s)
+	assert.Assert(t, &s != ptr)
+
+	s = ""
+	ptr = ref.NewNillable(s)
+	assert.Assert(t, ptr == nil)
+}
+
+func TestDeref(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Returns the value referenced by the pointer", func(t *testing.T) {
+		s := "string"
+
+		assert.Equal(t, s, ref.Deref(&s))
+	})
+
+	t.Run("Panics if a nil value is received", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("ref.Deref did not panic.")
+			}
+		}()
+		ref.Deref[int](nil)
+	})
+}
+
+func TestDerefZero(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Returns the underlying value of the pointer", func(t *testing.T) {
+		s := "string"
+		assert.Equal(t, ref.DerefZero(&s), "string")
+	})
+
+	t.Run("Returns the default value if the pointer is nil", func(t *testing.T) {
+		assert.Equal(t, ref.DerefZero[string](nil), "")
+	})
+}
+
+func TestUnsignedPtr(t *testing.T) {
+	t.Parallel()
+
+	t.Run("*int to *uint", func(t *testing.T) {
+		s := int(4)
+		u := uint(4)
+		assert.DeepEqual(t, ref.UnsignedPtr[int, uint](&s), &u)
+	})
+
+	t.Run("*int (nil) to *uint (nil)", func(t *testing.T) {
+		var s *int
+		var u *uint
+		assert.DeepEqual(t, ref.UnsignedPtr[int, uint](s), u)
+	})
+
+	t.Run("*int8 to *uint8", func(t *testing.T) {
+		s := int8(1)
+		u := uint8(1)
+		assert.DeepEqual(t, ref.UnsignedPtr[int8, uint8](&s), &u)
+	})
+
+	t.Run("*int8 (nil) to *uint8 (nil)", func(t *testing.T) {
+		var s *int8
+		var u *uint8
+		assert.DeepEqual(t, ref.UnsignedPtr[int8, uint8](s), u)
+	})
+}

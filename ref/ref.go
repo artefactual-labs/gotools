f44525e0ab800a96a47e5e5cd27729d5f1c08444
Jesús García Crespo <jesus@sevein.com>
2023-06-28 16:30:18 +0000

    ref: add initial implementation

---
+++ ref/ref.go
@@ -0,0 +1,45 @@
+// Package ref provides utility functions for handling pointers in Go.
+package ref
+
+import "golang.org/x/exp/constraints"
+
+// New returns a pointer to x.
+func New[T any](x T) *T {
+	return &x
+}
+
+// NewNillable returns a pointer to x.
+// It returns nil when x has the zero value of T
+func NewNillable[T comparable](x T) *T {
+	var z T
+	if x == z {
+		return nil
+	}
+	return &x
+}
+
+// Deref returns the value stored at the pointer x.
+// It panics if x is nil.
+func Deref[T any](x *T) T {
+	return *x
+}
+
+// DerefZero returns the value stored at the pointer x.
+// It returns the zero value of T when the pointer is nil.
+func DerefZero[T any](x *T) T {
+	if x == nil {
+		var z T
+		return z
+	}
+	return *x
+}
+
+// UnsignedPtr converts a signed integer into an unsigned integer.
+func UnsignedPtr[S constraints.Signed, U constraints.Unsigned](s *S) *U {
+	var ret *U
+	if s != nil {
+		u := U(*s)
+		ret = &u
+	}
+	return ret
+}
